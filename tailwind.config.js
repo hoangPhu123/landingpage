db8b1aa94144e1cc48ceeb1fa5460076edc64368
hoangPhu123 <phuvictor3979@gmail.com>
2024-03-07 22:23:07 +0700

    setup environment sources

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    minWidth: {
+      150: "9.5rem",
+    },
+  },
+  plugins: [],
+};
